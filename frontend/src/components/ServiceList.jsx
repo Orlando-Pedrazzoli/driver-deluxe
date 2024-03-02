905e3adb002c8234fcf8dcdb14fc35ee7fd119e8
Guilherme Carvalho <guicarv99@gmail.com>
2024-03-02 15:35:13 +0000

    fetching info from backend working

--- frontend/src/components/ServiceList.jsx
+++ frontend/src/components/ServiceList.jsx
@@ -1,96 +1,46 @@
 import { Link } from 'react-router-dom';
-import massage1 from '../assets/vibration.png';
-import massage2 from '../assets/reflexology.png';
-import massage3 from '../assets/massage1.png';
-import massage4 from '../assets/chairmassage.png';
+import { getAllServices } from '../api/services.api';
+import { useState, useEffect } from 'react';
 
 function ServiceList() {
+  const [services, setServices] = useState([]);
+
+  const fetchServices = async () => {
+    try {
+      const response = await getAllServices();
+      setServices(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error fetching services:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchServices();
+  }, []);
+
   return (
-    <section>
-      <div className='mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-2'>
-        <header className='text-center'>
-          <h2 className='text-xl font-bold text-gray-900 sm:text-3xl'>
-            Therapeutic Massages
-          </h2>
+    <div className='ServiceList'>
+      <h1>All Services</h1>
 
-          <p className='mx-auto mt-4 max-w-md text-gray-500'>
-            Therapeutic Massage applies pressure on the energetic lines and
-            points more intense than the Massage for relaxation.
-          </p>
-        </header>
-
-        <ul className='mt-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-4'>
-          <li>
-            <Link to={`/services/${serviceId}`}>
-              <img
-                src={massage1}
-                alt=''
-                className='h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]'
-              />
-
-              <div className='relative bg-white pt-3'>
-                <h3 className='text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4'>
-                  Reflexology
-                </h3>
-              </div>
+      {services.map(service => {
+        return (
+          <div key={service._id} className='ServiceCard card'>
+            <Link to={`/services/${service._id}`}>
+              <h3>{service.serviceName}</h3>
+              {service.imgURL && (
+                <img
+                  src={service.imgURL}
+                  width={100}
+                  alt={service.serviceName}
+                />
+              )}
+              <p>{service.address}</p>
             </Link>
-          </li>
-
-          <li>
-            <a href='#' className='group block overflow-hidden'>
-              <img
-                src={massage2}
-                alt=''
-                className='h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]'
-              />
-
-              <div className='relative bg-white pt-3'>
-                <h3 className='text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4'>
-                  Shiatsu
-                </h3>
-              </div>
-            </a>
-          </li>
-
-          <li>
-            <a href='#' className='group block overflow-hidden'>
-              <img
-                src={massage3}
-                alt=''
-                className='h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]'
-              />
-
-              <div className='relative bg-white pt-3'>
-                <h3 className='text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4'>
-                  Vibration
-                </h3>
-              </div>
-            </a>
-          </li>
-
-          <li>
-            <a href='#' className='group block overflow-hidden'>
-              <img
-                src={massage4}
-                alt=''
-                className='h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]'
-              />
-
-              <div className='relative bg-white pt-3'>
-                <h3 className='text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4'>
-                  Chair Massage
-                </h3>
-              </div>
-            </a>
-          </li>
-        </ul>
-        <span className='flex items-center mt-28'>
-          <span className='h-px flex-1 bg-gray-300'></span>
-          <span className='shrink-0 px-6'></span>
-          <span className='h-px flex-1 bg-gray-300'></span>
-        </span>
-      </div>
-    </section>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
--- frontend/src/pages/SingleService.jsx
+++ frontend/src/pages/SingleService.jsx
@@ -1,10 +1,9 @@
 import { getService } from '../api/services.api';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 
 function SingleService() {
-  const [services, setServices] = useState({});
+  const [service, setService] = useState({});
 
   const { serviceId } = useParams();
 
@@ -12,7 +11,7 @@ function SingleService() {
     try {
       const response = await getService(serviceId);
       console.log(response.data);
-      setServices(response.data);
+      setService(response.data);
     } catch (error) {
       console.log(error);
     }
@@ -24,18 +23,13 @@ function SingleService() {
 
   return (
     <div className='ServiceList'>
-      <h1>Services</h1>
-
-      {services.map(service => (
-        <div key={service._id} className='ServiceCard card'>
-          <Link to={`/services/${service._id}`}>
-            <h3>{service.serviceName}</h3>
-            {service.imgUrl && (
-              <img src={service.imgUrl} width={100} alt={service.serviceName} />
-            )}
-          </Link>
-        </div>
-      ))}
+      {service && (
+        <>
+          <h1>{service.serviceName}</h1>
+          <p>{service.description}</p>
+          <img src={service.imgURL} alt='' />
+        </>
+      )}
     </div>
   );
 }
