af8395a2a82a53fb826e60a636e05b923f01d47b
Guilherme Carvalho <guicarv99@gmail.com>
2024-03-02 16:18:32 +0000

    service massage types page done

--- frontend/src/components/ServiceList.jsx
+++ frontend/src/components/ServiceList.jsx
@@ -20,27 +20,53 @@ function ServiceList() {
   }, []);
 
   return (
-    <div className='ServiceList'>
-      <h1>All Services</h1>
+    <section>
+      <div className='mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8'>
+        <header className='text-center'>
+          <h2 className='text-xl font-bold text-gray-900 sm:text-3xl'>
+            Product Collection
+          </h2>
+          <p className='mx-auto mt-4 max-w-md text-gray-500'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque
+            praesentium cumque iure dicta incidunt est ipsam, officia dolor
+            fugit natus?
+          </p>
+        </header>
 
-      {services.map(service => {
-        return (
-          <div key={service._id} className='ServiceCard card'>
-            <Link to={`/services/${service._id}`}>
-              <h3>{service.serviceName}</h3>
-              {service.imgURL && (
-                <img
-                  src={service.imgURL}
-                  width={100}
-                  alt={service.serviceName}
-                />
-              )}
-              <p>{service.address}</p>
-            </Link>
-          </div>
-        );
-      })}
-    </div>
+        <div className='mt-8 flex'>
+          {services &&
+            services.length > 0 &&
+            [...new Set(services.map(service => service.serviceType))].map(
+              serviceType => {
+                const serviceWithType = services.find(
+                  service => service.serviceType === serviceType
+                );
+
+                return (
+                  <div
+                    key={serviceType}
+                    className='w-1/4 relative bg-white pt-3 mr-4'
+                  >
+                    <Link to={`/services?type=${serviceType}`}>
+                      <h3>{serviceType}</h3>
+                      {serviceWithType && serviceWithType.imgURL && (
+                        <img
+                          src={serviceWithType.imgURL}
+                          alt={serviceType}
+                          className='h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]'
+                        />
+                      )}
+                    </Link>
+                    <h3 className='text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4'>
+                      {services.serviceName}
+                    </h3>
+                  </div>
+                );
+              }
+            )}
+        </div>
+      </div>
+    </section>
   );
 }
 
