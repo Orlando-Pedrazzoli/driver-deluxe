6b94d8821200375b535e32ba4ce3af0243a6d54c
Guilherme Carvalho <guicarv99@gmail.com>
2024-03-20 21:20:23 +0000

    News api working on the frontend and backend

--- frontend/src/pages/News.jsx
+++ frontend/src/pages/News.jsx
@@ -1,210 +1,43 @@
-import React from 'react';
-import tvde1_img from '../assets/tvde1.jpg';
-import tvde2_img from '../assets/tvde2.jpg';
-import tvde3_img from '../assets/tvde3.jpg';
-import tvde4_img from '../assets/tvde4.jpg';
-import tvde5_img from '../assets/tvde5.jpg';
-import tvde6_img from '../assets/tvde6.jpg';
-import tvde7_img from '../assets/tvde7.jpg';
+import { useState, useEffect } from 'react';
+import { getNews } from '@/api/news.api';
 
 function News() {
+  const [news, setNews] = useState([]);
+
+  const getArticles = async () => {
+    try {
+      const response = await getNews();
+      console.log(response.data);
+      setNews(response.data.articles);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
   return (
-    <section className='dark:dark:bg-gray-800 dark:dark:text-gray-100'>
-      <div className='container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12'>
-        <a
-          rel='noopener noreferrer'
-          href='#'
-          className='block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:dark:bg-gray-900'
-        >
-          <img
-            src={tvde1_img}
-            alt=''
-            className='object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:dark:bg-gray-500'
-          />
-          <div className='p-6 space-y-2 lg:col-span-5'>
-            <h3 className='text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline'>
-              News and Articles
-            </h3>
-            <span className='text-xs dark:dark:text-gray-400'>
-              March 4, 2024
-            </span>
-            <p>
-              The strike, which kicked off this morning, has left passengers who
-              rely on Uber and Bolt stranded. An ITWeb staffer who uses Uber
-              says they waited for the transport for more than 30 minutes, with
-              the Uber driver, who was not part of the strike, saying they had
-              “risked” their life by continuing to operate while other drivers
-              were on strike. According to the employee, the fee for the ride to
-              work was triple the amount normally paid. Amid the industrial
-              action, the drivers will go to all Uber and Bolt offices −
-              including their airport holding areas − to demand their grievances
-              be addressed, says the E-Hailing Partners Council, which
-              represents Uber and Bolt drivers.
-            </p>
-          </div>
-        </a>
-        <div className='grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'>
-          <a
-            rel='noopener noreferrer'
-            href='#'
-            className='max-w-sm mx-auto group hover:no-underline focus:no-underline dark:dark:bg-gray-900'
-          >
-            <img
-              role='presentation'
-              className='object-cover w-full rounded h-44 dark:dark:bg-gray-500'
-              src={tvde2_img}
-            />
-            <div className='p-6 space-y-2'>
-              <h3 className='text-2xl font-semibold group-hover:underline group-focus:underline'>
-                In usu laoreet repudiare legendos
-              </h3>
-              <span className='text-xs dark:dark:text-gray-400'>
-                January 21, 2021
-              </span>
-              <p>
-                Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
-                neglegentur, ex has tantas percipit perfecto. At per tempor
-                albucius perfecto, ei probatus consulatu patrioque mea, ei
-                vocent delicata indoctum pri.
-              </p>
+    <>
+      <div>
+        <h1>Latest News</h1>
+        {news &&
+          news.map((article, index) => (
+            <div key={index}>
+              <h2>{article.title}</h2>
+              <p>Author: {article.author}</p>
+              <p>Published at: {article.publishedAt}</p>
+              {article.url && <a href={article.url}>Read More</a>}
+              {article.urlToImage && (
+                <img src={article.urlToImage} alt='Article' />
+              )}
+              <p>{article.content}</p>
+              <hr />
             </div>
-          </a>
-          <a
-            rel='noopener noreferrer'
-            href='#'
-            className='max-w-sm mx-auto group hover:no-underline focus:no-underline dark:dark:bg-gray-900'
-          >
-            <img
-              role='presentation'
-              className='object-cover w-full rounded h-44 dark:dark:bg-gray-500'
-              src={tvde3_img}
-            />
-            <div className='p-6 space-y-2'>
-              <h3 className='text-2xl font-semibold group-hover:underline group-focus:underline'>
-                In usu laoreet repudiare legendos
-              </h3>
-              <span className='text-xs dark:dark:text-gray-400'>
-                January 22, 2021
-              </span>
-              <p>
-                Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
-                neglegentur, ex has tantas percipit perfecto. At per tempor
-                albucius perfecto, ei probatus consulatu patrioque mea, ei
-                vocent delicata indoctum pri.
-              </p>
-            </div>
-          </a>
-          <a
-            rel='noopener noreferrer'
-            href='#'
-            className='max-w-sm mx-auto group hover:no-underline focus:no-underline dark:dark:bg-gray-900'
-          >
-            <img
-              role='presentation'
-              className='object-cover w-full rounded h-44 dark:dark:bg-gray-500'
-              src={tvde4_img}
-            />
-            <div className='p-6 space-y-2'>
-              <h3 className='text-2xl font-semibold group-hover:underline group-focus:underline'>
-                In usu laoreet repudiare legendos
-              </h3>
-              <span className='text-xs dark:dark:text-gray-400'>
-                January 23, 2021
-              </span>
-              <p>
-                Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
-                neglegentur, ex has tantas percipit perfecto. At per tempor
-                albucius perfecto, ei probatus consulatu patrioque mea, ei
-                vocent delicata indoctum pri.
-              </p>
-            </div>
-          </a>
-          <a
-            rel='noopener noreferrer'
-            href='#'
-            className='max-w-sm mx-auto group hover:no-underline focus:no-underline dark:dark:bg-gray-900 hidden sm:block'
-          >
-            <img
-              role='presentation'
-              className='object-cover w-full rounded h-44 dark:dark:bg-gray-500'
-              src={tvde5_img}
-            />
-            <div className='p-6 space-y-2'>
-              <h3 className='text-2xl font-semibold group-hover:underline group-focus:underline'>
-                In usu laoreet repudiare legendos
-              </h3>
-              <span className='text-xs dark:dark:text-gray-400'>
-                January 24, 2021
-              </span>
-              <p>
-                Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
-                neglegentur, ex has tantas percipit perfecto. At per tempor
-                albucius perfecto, ei probatus consulatu patrioque mea, ei
-                vocent delicata indoctum pri.
-              </p>
-            </div>
-          </a>
-          <a
-            rel='noopener noreferrer'
-            href='#'
-            className='max-w-sm mx-auto group hover:no-underline focus:no-underline dark:dark:bg-gray-900 hidden sm:block'
-          >
-            <img
-              role='presentation'
-              className='object-cover w-full rounded h-44 dark:dark:bg-gray-500'
-              src={tvde6_img}
-            />
-            <div className='p-6 space-y-2'>
-              <h3 className='text-2xl font-semibold group-hover:underline group-focus:underline'>
-                In usu laoreet repudiare legendos
-              </h3>
-              <span className='text-xs dark:dark:text-gray-400'>
-                January 25, 2021
-              </span>
-              <p>
-                Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
-                neglegentur, ex has tantas percipit perfecto. At per tempor
-                albucius perfecto, ei probatus consulatu patrioque mea, ei
-                vocent delicata indoctum pri.
-              </p>
-            </div>
-          </a>
-          <a
-            rel='noopener noreferrer'
-            href='#'
-            className='max-w-sm mx-auto group hover:no-underline focus:no-underline dark:dark:bg-gray-900 hidden sm:block'
-          >
-            <img
-              role='presentation'
-              className='object-cover w-full rounded h-44 dark:dark:bg-gray-500'
-              src={tvde7_img}
-            />
-            <div className='p-6 space-y-2'>
-              <h3 className='text-2xl font-semibold group-hover:underline group-focus:underline'>
-                In usu laoreet repudiare legendos
-              </h3>
-              <span className='text-xs dark:dark:text-gray-400'>
-                January 26, 2021
-              </span>
-              <p>
-                Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
-                neglegentur, ex has tantas percipit perfecto. At per tempor
-                albucius perfecto, ei probatus consulatu patrioque mea, ei
-                vocent delicata indoctum pri.
-              </p>
-            </div>
-          </a>
-        </div>
-        <div className='flex justify-center'>
-          <button
-            type='button'
-            className='px-6 py-3 text-sm rounded-md hover:underline dark:dark:bg-gray-900 dark:dark:text-gray-400'
-          >
-            Load more posts...
-          </button>
-        </div>
+          ))}
       </div>
-    </section>
+    </>
   );
 }
 
