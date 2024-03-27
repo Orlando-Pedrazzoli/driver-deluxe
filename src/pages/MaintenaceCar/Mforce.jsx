4f9b138bed9267bbebfec78fc28133e6136a4da3
Guilherme Carvalho <guicarv99@gmail.com>
2024-03-27 21:17:16 +0000

    Mforce loading added

--- src/pages/MaintenaceCar/Mforce.jsx
+++ src/pages/MaintenaceCar/Mforce.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 
 function Mforce() {
   const [services, setServices] = useState();
+  const [loading, setLoading] = useState(true);
 
   const getServices = async () => {
     try {
       const response = await getService('mforce');
-      console.log(response);
-      console.log(response.data);
       setServices(response.data);
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -21,7 +21,12 @@ function Mforce() {
     getServices();
   }, []);
 
-  return (
+  return loading ? (
+    <div className='flex flex-col justify-center items-center'>
+      <img src='/src/assets/LoadingNews.gif' alt='Loading...' />
+      <p>Loading...</p>
+    </div>
+  ) : (
     <>
       <section className='bg-white text-gray-100 mt-12'>
         <div className='container flex flex-col-reverse mx-auto lg:flex-row'>
